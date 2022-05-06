3182c55fede554246400d6dfe894fbaf3bc37ff6
MaxiTRR <maxihtorres@gmail.com>
2022-05-06 00:47:30 -0300

    Primer commit del Ejercicio de Task List Argentina Programa

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import { Task } from "./Task"
+ 
+export const TASKS:Task[] = [
+        {
+            id: 1,
+            text: "Terminar primer modulo de Angular",
+            day: "Agosto 5 a la 12:00",
+            reminder:true
+        },
+        {
+            id: 2,
+            text: "Hacer ejercicio de Bootstrap",
+            day: "Agosto 5 a la 17:00",
+            reminder:true
+        },
+        {
+            id: 3,
+            text: "Salir a correr",
+            day: "Agosto 5 a la 20:00",
+            reminder:false
+        },
+        {
+            id: 4,
+            text: "Seguir con el Portfolio",
+            day: "Agosto 5 a la 23:00",
+            reminder:false
+        }
+
+    ]
